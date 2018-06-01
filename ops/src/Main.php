0244a27aab2189caae261938fb383087499f28d2
mohamedoo9 <39798188+mohamedoo9@users.noreply.github.com>
2018-06-01 23:14:02 +0300

    Update Main.php

--- ops/src/Main.php
+++ ops/src/Main.php
@@ -5,11 +5,20 @@ namespace ops;
 use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class Main extends PluginBase{
 
    public function onEnable(){
        $this->getLogger()->info(TextFormat::GREEN."plugin by mohamedoo9 has been enabled");
    }
+   
+   public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+      switch($cmd->getName()){
+         case 'oo':
+            if($sender->isOp()){
+            $sender->sendMessage(TextFormat::.BLUE."test my plugin");
+     }else{         
+   }
 }
- 
